592c5fae2534c1abd6372100ded592e1f38e80dd
Fan Zhang <fan.zh.0518@gmail.com>
2023-12-04 09:53:25 -0700

    v0.3.1
    file cleanup

--- MoPanda/archived/predictor_test.py
+++
@@ -1,8 +0,0 @@
-from EDA.MoPanda.MoPanda.modules.inwellpredictor import InWellPredictor
-from EDA.MoPanda.MoPanda.modules.las_io import LasIO
-
-las_file_path: str = '../data/las/Denova1_modified.LAS'
-
-# Load LAS file
-log = LasIO(las_file_path)
-InWellPredictor(log)
